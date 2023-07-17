25e10e6bde2efb7b7d0e1a07f29845b7efefa6f0
hiennv05 <bg.hien@gmail.com>
2023-07-17 12:51:50 +0900

    Start 1

---
+++ Backend/src/main/java/com/vti/BookingHosBackend/BookingHosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vti.BookingHosBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingHosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingHosBackendApplication.class, args);
+	}
+
+}
